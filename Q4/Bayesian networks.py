9882fa212cf72f97ca8b916bb78e8d6e81b1a059
Manishreddy230 <manishchintam23@gmail.com>
2026-05-29 22:56:50 +0530

    Update Bayesian networks.py

--- Q4/Bayesian networks.py
+++ Q4/Bayesian networks.py
@@ -1,89 +1,64 @@
-from pgmpy.models import DiscreteBayesianNetwork
-from pgmpy.factors.discrete import TabularCPD
-from pgmpy.inference import VariableElimination
+P_Burglary = {
+    True: 0.001,
+    False: 0.999
+}
 
-model = DiscreteBayesianNetwork([
-    ("Burglary", "Alarm"),
-    ("Earthquake", "Alarm"),
-    ("Alarm", "JohnCalls"),
-    ("Alarm", "MaryCalls")
-])
+P_Earthquake = {
+    True: 0.002,
+    False: 0.998
+}
 
-cpd_burglary = TabularCPD(
-    variable="Burglary",
-    variable_card=2,
-    values=[[0.999], [0.001]]
-)
+P_Alarm = {
+    (True, True): 0.95,
+    (True, False): 0.94,
+    (False, True): 0.29,
+    (False, False): 0.001
+}
 
-cpd_earthquake = TabularCPD(
-    variable="Earthquake",
-    variable_card=2,
-    values=[[0.998], [0.002]]
-)
+P_JohnCalls = {
+    True: 0.90,
+    False: 0.05
+}
 
-cpd_alarm = TabularCPD(
-    variable="Alarm",
-    variable_card=2,
-    values=[
-        [0.999, 0.71, 0.06, 0.05],
-        [0.001, 0.29, 0.94, 0.95]
-    ],
-    evidence=["Burglary", "Earthquake"],
-    evidence_card=[2, 2]
-)
+P_MaryCalls = {
+    True: 0.70,
+    False: 0.01
+}
 
-cpd_john = TabularCPD(
-    variable="JohnCalls",
-    variable_card=2,
-    values=[
-        [0.95, 0.10],
-        [0.05, 0.90]
-    ],
-    evidence=["Alarm"],
-    evidence_card=[2]
-)
 
-cpd_mary = TabularCPD(
-    variable="MaryCalls",
-    variable_card=2,
-    values=[
-        [0.99, 0.30],
-        [0.01, 0.70]
-    ],
-    evidence=["Alarm"],
-    evidence_card=[2]
-)
+def probability_burglary_given_calls():
 
-model.add_cpds(
-    cpd_burglary,
-    cpd_earthquake,
-    cpd_alarm,
-    cpd_john,
-    cpd_mary
-)
+    numerator = 0
+    denominator = 0
 
-print("Model Valid:", model.check_model())
+    for burglary in [True, False]:
+        for earthquake in [True, False]:
 
-inference = VariableElimination(model)
+            p_b = P_Burglary[burglary]
+            p_e = P_Earthquake[earthquake]
 
-print("\nProbability of Burglary:")
-result1 = inference.query(
-    variables=["Burglary"]
-)
-print(result1)
+            p_a = P_Alarm[(burglary, earthquake)]
 
-print("\nProbability of Alarm:")
-result2 = inference.query(
-    variables=["Alarm"]
-)
-print(result2)
+            p_j = P_JohnCalls[True]
+            p_m = P_MaryCalls[True]
+
+            joint = p_b * p_e * p_a * p_j * p_m
+
+            denominator += joint
+
+            if burglary:
+                numerator += joint
+
+    return numerator / denominator
+
+
+print("Bayesian Network Example")
+print("------------------------")
+
+print("P(Burglary=True) =", P_Burglary[True])
+print("P(Earthquake=True) =", P_Earthquake[True])
+
+result = probability_burglary_given_calls()
 
 print("\nProbability of Burglary given JohnCalls and MaryCalls:")
-result3 = inference.query(
-    variables=["Burglary"],
-    evidence={
-        "JohnCalls": 1,
-        "MaryCalls": 1
-    }
-)
-print(result3)
+print(round(result, 4))
